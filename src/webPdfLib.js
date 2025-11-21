f5d515215006dc169c1f7b2a704490a383f685b9
hyunsu00 <hyunsu00@hancom.com>
2025-11-21 09:41:27 +0900

    작업중

--- src/webPdfLib.js
+++ src/webPdfLib.js
@@ -72,66 +72,66 @@ export default (function () {
 
           console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdf-annotate.js) Succeeded`);
 
-          import(/* webpackIgnore: true */ `${lipsPath}/pdfjs/web/viewer.js`)
-            .then((module) => {
-              this.PDFViewerApplication = window.PDFViewerApplication;
-              this.PDFAppOptions = window.PDFViewerApplicationOptions;
-              this.gUndoRedoManager = new UndoRedoManager(this.PDFViewerApplication.baseUrl);
-              const _appOptions = this.PDFAppOptions;
-              _appOptions.set('defaultUrl', `${defaultUrl}`); // 절대경로
-              _appOptions.set('documentName', documentName || '');
-              _appOptions.set('disablePreferences', true);
-              _appOptions.set('scrollModeOnLoad', 0);
-              _appOptions.set('workerSrc', `${lipsPath}/pdfjs/build/pdf.worker.js`); // 절대경로
-              _appOptions.set('sandboxBundleSrc', `${lipsPath}/pdfjs/build/pdf.sandbox.js`); // 절대경로
-              _appOptions.set("imageResourcesPath", `${lipsPath}/pdfjs/web/images/`); // 절대경로
-              _appOptions.set('documentloaded', PDFJsListener.onDocumentLoaded);
-              _appOptions.set('documentSave', PDFJsListener.onDocumentSave);
-              _appOptions.set('pdf-annotate-render', PDFJsListener.onWebViewerAnnotateRender);
-              _appOptions.set('pdf-annotate-thumnailRender', PDFJsListener.onWebViewerAnnotateThumnailRender);
-              _appOptions.set('updateUi', PDFJsListener.onUpdateUi);
-              _appOptions.set('makeUpdateEventAction', PDFJsListener.onMakeUpdateEventAction);
-              _appOptions.set('sidebarButtonStatus', PDFJsListener.onSidebarButtonStatus);
-              _appOptions.set('setEnable', PDFJsListener.onSetEnable);
-              _appOptions.set('updateDescription', PDFJsListener.onUpdateDescription);
-              _appOptions.set('setUIDisableState', PDFJsListener.onSetUIDisableState);
-              _appOptions.set('webViewerUpdateViewarea', PDFJsListener.onWebViewerUpdateViewarea);
-              _appOptions.set('enablePermissions', true);
-              _appOptions.set('initPasswordDialog', PDFJsListener.onInitPasswordDialog);
-              _appOptions.set('closeDialog', PDFJsListener.onCloseDialog);
-              _appOptions.set('showDialog', PDFJsListener.onShowDialog);
-              _appOptions.set('showAlertModalDialog', PDFJsListener.onShowAlertModalDialog);
-              _appOptions.set('hideLoadingProgress', PDFJsListener.onHideLoadingProgress);
-              _appOptions.set('showLoadingProgress', PDFJsListener.onShowLoadingProgress);
-              _appOptions.set('rotationchanging', PDFJsListener.onRotationChanging);
-              renderExternalAnnotations && _appOptions.set('renderExternalAnnotations', PDFJsListener.onRenderExternalAnnotations);
-              streaming && _appOptions.set('streaming', PDFJsListener.onStreaming);
+          // import(/* webpackIgnore: true */ `${lipsPath}/pdfjs/web/viewer.js`)
+          //   .then((module) => {
+          //     this.PDFViewerApplication = window.PDFViewerApplication;
+          //     this.PDFAppOptions = window.PDFViewerApplicationOptions;
+          //     this.gUndoRedoManager = new UndoRedoManager(this.PDFViewerApplication.baseUrl);
+          //     const _appOptions = this.PDFAppOptions;
+          //     _appOptions.set('defaultUrl', `${defaultUrl}`); // 절대경로
+          //     _appOptions.set('documentName', documentName || '');
+          //     _appOptions.set('disablePreferences', true);
+          //     _appOptions.set('scrollModeOnLoad', 0);
+          //     _appOptions.set('workerSrc', `${lipsPath}/pdfjs/build/pdf.worker.js`); // 절대경로
+          //     _appOptions.set('sandboxBundleSrc', `${lipsPath}/pdfjs/build/pdf.sandbox.js`); // 절대경로
+          //     _appOptions.set("imageResourcesPath", `${lipsPath}/pdfjs/web/images/`); // 절대경로
+          //     _appOptions.set('documentloaded', PDFJsListener.onDocumentLoaded);
+          //     _appOptions.set('documentSave', PDFJsListener.onDocumentSave);
+          //     _appOptions.set('pdf-annotate-render', PDFJsListener.onWebViewerAnnotateRender);
+          //     _appOptions.set('pdf-annotate-thumnailRender', PDFJsListener.onWebViewerAnnotateThumnailRender);
+          //     _appOptions.set('updateUi', PDFJsListener.onUpdateUi);
+          //     _appOptions.set('makeUpdateEventAction', PDFJsListener.onMakeUpdateEventAction);
+          //     _appOptions.set('sidebarButtonStatus', PDFJsListener.onSidebarButtonStatus);
+          //     _appOptions.set('setEnable', PDFJsListener.onSetEnable);
+          //     _appOptions.set('updateDescription', PDFJsListener.onUpdateDescription);
+          //     _appOptions.set('setUIDisableState', PDFJsListener.onSetUIDisableState);
+          //     _appOptions.set('webViewerUpdateViewarea', PDFJsListener.onWebViewerUpdateViewarea);
+          //     _appOptions.set('enablePermissions', true);
+          //     _appOptions.set('initPasswordDialog', PDFJsListener.onInitPasswordDialog);
+          //     _appOptions.set('closeDialog', PDFJsListener.onCloseDialog);
+          //     _appOptions.set('showDialog', PDFJsListener.onShowDialog);
+          //     _appOptions.set('showAlertModalDialog', PDFJsListener.onShowAlertModalDialog);
+          //     _appOptions.set('hideLoadingProgress', PDFJsListener.onHideLoadingProgress);
+          //     _appOptions.set('showLoadingProgress', PDFJsListener.onShowLoadingProgress);
+          //     _appOptions.set('rotationchanging', PDFJsListener.onRotationChanging);
+          //     renderExternalAnnotations && _appOptions.set('renderExternalAnnotations', PDFJsListener.onRenderExternalAnnotations);
+          //     streaming && _appOptions.set('streaming', PDFJsListener.onStreaming);
               
-              console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdfjs/web/viewer.js) Succeeded`);
-            })
-            .catch((err) => {
-              console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdfjs/web/viewer.js) Failed] : ${err.message}`);
-            });
+          //     console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdfjs/web/viewer.js) Succeeded`);
+          //   })
+          //   .catch((err) => {
+          //     console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdfjs/web/viewer.js) Failed] : ${err.message}`);
+          //   });
 
-          import(/* webpackIgnore: true */ `${lipsPath}/annotpdf.js`)
-            .then((module) => {
-              this.PDFAnnotateWriter = window.pdfAnnotate;
+          // import(/* webpackIgnore: true */ `${lipsPath}/annotpdf.js`)
+          //   .then((module) => {
+          //     this.PDFAnnotateWriter = window.pdfAnnotate;
 
-              console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/annotpdf.js) Succeeded`);
-            })
-            .catch((err) => {
-              console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/annotpdf.js) Failed] : ${err.message}`);
-            });
+          //     console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/annotpdf.js) Succeeded`);
+          //   })
+          //   .catch((err) => {
+          //     console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/annotpdf.js) Failed] : ${err.message}`);
+          //   });
 
-          import(/* webpackIgnore: true */ `${lipsPath}/pdf-lib.js`)
-            .then((module) => {
-              this.PDFLib = window.PDFLib;
+          // import(/* webpackIgnore: true */ `${lipsPath}/pdf-lib.js`)
+          //   .then((module) => {
+          //     this.PDFLib = window.PDFLib;
 
-              console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdf-lib.js) Succeeded`);
-            })
-            .catch((err) => {
-              console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdf-lib.js) Failed] : ${err.message}`);
-            });
+          //     console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdf-lib.js) Succeeded`);
+          //   })
+          //   .catch((err) => {
+          //     console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdf-lib.js) Failed] : ${err.message}`);
+          //   });
         })
         .catch((err) => {
           console.log(`[import(/* webpackIgnore: true */ ${lipsPath}/pdf-annotate.js) Failed] : ${err.message}`);
