520639242b9fa7b20e58504c51bce16e69a35f0f
hyunsu00 <hyunsu00@hancom.com>
2025-11-30 21:11:51 +0900

    작업중

--- extern/pdf-annotate.js/test/mockGetAnnotations.js
+++
@@ -1,9 +0,0 @@
-export default function() {
-  return function(documentId, pageNumber) {
-    return Promise.resolve({
-      documentId,
-      pageNumber,
-      annotations: []
-    });
-  };
-};
